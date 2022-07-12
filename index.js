f522d9a9db6e99aaa8df1b55ee71da7a62f620cf
Daimonke <Daimonas98@gmail.com>
2022-07-12 21:55:33 +0300

    Register api

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import registerRouter from './routes/api/auth/register.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = process.env.PORT || 3020;
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    credentials: true,
+};
+
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use('/auth/register', registerRouter);
 
 
 app.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`))
---
+++ routes/api/auth/register.js
@@ -0,0 +1,65 @@
+import express from "express";
+import con from "../../../dbCon.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { username, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Passwords do not match",
+      });
+    }
+
+    if (username.length < 4 || password.length < 4) {
+      return res.status(400).json({
+        success: false,
+        message: "Username and password must be at least 4 characters long",
+      });
+    }
+
+    const [user] = await con.query(`SELECT * FROM users WHERE username = ?`, [
+      username,
+    ]);
+    if (user.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Username already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await con.query(
+      `INSERT INTO users (username, password) VALUES (?, ?)`,
+      [username, hashedPassword]
+    );
+    const token = jwt.sign(
+      { id: result.insertId, username: username },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 100 * 60 * 60 * 24 * 30,
+        sameSite: "strict",
+      })
+      .json({
+        success: true,
+        message: "User created successfully",
+      });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+export default router;
